7b24ada215c4b826640e2b1d7fe7859d82137774
walker <1424234500@qq.com>
2023-07-14 20:30:58 +0800

    LeeCodestayback

--- walker-core/src/main/java/com/walker/demo/leecode/LeeCodeStayBack.java
+++ walker-core/src/main/java/com/walker/demo/leecode/LeeCodeStayBack.java
@@ -23,10 +23,6 @@ public class LeeCodeStayBack {
 //    输出：true
 
     static class Solutionexist {
-        Map<String, Boolean> map = new HashMap<>();
-        String word;
-        char[][] board;
-        LinkedList<Character> route = new LinkedList<>();
 
         public static void main(String[] args) {
             new Solutionexist().exist(new char[][]{
@@ -35,9 +31,8 @@ public class LeeCodeStayBack {
                     , new char[]{'A', 'D', 'E', 'E'}
             }, "ABCCED");
         }
-
+        String word;
         public boolean exist(char[][] board, String word) {
-            this.board = board;
             this.word = word;
             Map<Character, Integer> map1 = new HashMap<>();
             for (int i = 0; i < board.length; i++) {
@@ -50,13 +45,14 @@ public class LeeCodeStayBack {
                     return false;
                 }
             }
-
+            Map<String, Boolean> map = new HashMap<>();
+            LinkedList<Character> route = new LinkedList<>();
 
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[i].length; j++) {
                     route.clear();
                     map.clear();
-                    if (go(i, j)) {
+                    if (go(route, map, board, i, j)) {
                         return true;
                     }
                 }
@@ -64,7 +60,7 @@ public class LeeCodeStayBack {
             return false;
         }
 
-        public boolean go(int i, int j) {
+        public boolean go(LinkedList<Character> route, Map<String, Boolean> map, char[][] board, int i, int j) {
             if (i >= board.length || i < 0) {
                 return false;
             }
@@ -91,16 +87,16 @@ public class LeeCodeStayBack {
                 return true;
             }
 
-            if (go(i, j + 1)) {
+            if (go(route, map, board, i, j + 1)) {
                 return true;
             }
-            if (go(i + 1, j)) {
+            if (go(route, map, board, i + 1, j)) {
                 return true;
             }
-            if (go(i, j - 1)) {
+            if (go(route, map, board, i, j - 1)) {
                 return true;
             }
-            if (go(i - 1, j)) {
+            if (go(route, map, board, i - 1, j)) {
                 return true;
             }
             route.removeLast();
@@ -109,5 +105,73 @@ public class LeeCodeStayBack {
         }
     }
 
+
+    //    剑指 Offer 13. 机器人的运动范围
+//    地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+//    输入：m = 2, n = 3, k = 1
+//    输出：3
+    static class SolutionmovingCount {
+        public static void main(String[] args) {
+            // new SolutionmovingCount().movingCount(11, 8, 16);
+            new SolutionmovingCount().movingCount(38, 15, 9);
+        }
+
+        // k 数位之和 而非 步数限制
+        public int movingCount(int m, int n, int k) {
+            Map<String, Boolean> map = new HashMap<>();
+            LinkedList<Integer> route = new LinkedList<>();
+            int[][] board = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    board[i][j] = 0;
+                }
+            }
+            go(route, map, board, 0, 0, k);
+//            return map.size();
+            int res = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    res += (board[i][j] > 0 ? 1 : 0);
+                }
+            }
+            return res;
+        }
+
+        public int getSum(int num) {
+            int sum = 0;
+            for (; num > 0; num /= 10)
+                sum += num % 10;
+            return sum;
+        }
+
+        public boolean go(LinkedList<Integer> route, Map<String, Boolean> map, int[][] board, int i, int j, int bitCount) {
+            if (i >= board.length || i < 0) {
+                return false;
+            }
+            if (j >= board[i].length || j < 0) {
+                return false;
+            }
+            if (map.containsKey(i + "_" + j)) {
+                return false;
+            }
+            if (getSum(i) + getSum(j) > bitCount) {
+                return false;
+            }
+            route.add(board[i][j]);
+            board[i][j]++;
+            map.put(i + "_" + j, true);
+
+
+            go(route, map, board, i, j + 1, bitCount);
+            go(route, map, board, i + 1, j, bitCount);
+//        go(route, map, board, i, j - 1, bitCount);
+//        go(route, map, board, i - 1, j, bitCount);
+
+            route.removeLast();
+            map.remove(i + "_" + j);
+
+            return false;
+        }
+    }
 
 }
